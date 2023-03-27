6dd0ac49a863ded25a90ccd277b6ef277b062273
Mateus de Freitas <106099290+MateusTestingCompany@users.noreply.github.com>
2023-03-27 16:18:59 -0300

    Add status code

--- src/server.js
+++ src/server.js
@@ -12,24 +12,22 @@ const server = http.createServer((req, res) => {
 
     if (method === 'GET' && url === '/users') {
         return res
-        .setHeader('Content-type', 'application/json') //informar o tipo de response
-        .end(JSON.stringify(users))
+            .setHeader('Content-type', 'application/json') //informar o tipo de response
+            .end(JSON.stringify(users))
     }
 
     if (method === 'POST' && url === '/users') {
-
         //adicionar um usuário
         users.push({
             id: 1,
             name: 'Fulano',
             email: 'fulano@gmail.com'
 
-    })
-
-        return res.end('Criação de usuários')
+        })
+        return res.writeHead(201).end() //add status code 201
     }
 
-    return res.end('Hello Ignite')
+    return res.writeHead(404).end()
 })
 
 server.listen(3333) //localhost:3333
